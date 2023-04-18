636bf8bf6c759a2030e4f23114dfbba05a236d21
joshuapent <joshdavidpent@gmail.com>
2023-04-17 20:16:29 -0500

    Actually finished with no errors

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -4,16 +4,25 @@ import {useState} from 'react';
 
 function NewSkillForm(props) {
 
-    const [newSkill, setNewSkill] = useState("");
+    const [newSkill, setNewSkill] = useState({name: "", level: 3});
 
     function handleChange(evt) {
-      setNewSkill(evt.target.value)
-    }
+      // console.log(newSkill)
+      // console.log(evt.target)
+      // console.log(evt.target.input)
+      if (evt.target.name === 'Skill') {
+        setNewSkill({level: newSkill.level, name: evt.target.value})
+        console.log(newSkill)
+      } else {
+        setNewSkill({level: evt.target.value, name: newSkill.name})
+        console.log(newSkill)
+      }
+    } 
 
     function handleAdd(evt) {
       evt.preventDefault()
       props.handleAddSkill({level: evt.target.Level.value, name: evt.target.Skill.value})
-      setNewSkill("");
+      setNewSkill({name: "", level: 3});
 
     }
 
@@ -25,13 +34,13 @@ function NewSkillForm(props) {
               name="Skill"
               type="text"
               placeholder="New Skill"
-              value={newSkill}
+              value={newSkill.name}
               onChange={handleChange}
               required
               pattern=".{3,}"
             />
             <label htmlFor="Level" value="">Level</label>
-            <select name="Level" id="">
+            <select name="Level" id="" value={newSkill.level} onChange={handleChange}>
                 <option value={1}>1</option>
                 <option value={2}>2</option>
                 <option value={3}>3</option>
