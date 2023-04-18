1984eb385b0341d21b957b446a5e80e5c40d6dab
joshuapent <joshdavidpent@gmail.com>
2023-04-18 15:10:49 -0500

    Updated

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -7,21 +7,12 @@ function NewSkillForm(props) {
     const [newSkill, setNewSkill] = useState({name: "", level: 3});
 
     function handleChange(evt) {
-      // console.log(newSkill)
-      // console.log(evt.target)
-      // console.log(evt.target.input)
-      if (evt.target.name === 'Skill') {
-        setNewSkill({level: newSkill.level, name: evt.target.value})
-        console.log(newSkill)
-      } else {
-        setNewSkill({level: evt.target.value, name: newSkill.name})
-        console.log(newSkill)
-      }
+      setNewSkill({...newSkill, [evt.target.name]: evt.target.value})
     } 
 
     function handleAdd(evt) {
       evt.preventDefault()
-      props.handleAddSkill({level: evt.target.Level.value, name: evt.target.Skill.value})
+      props.handleAddSkill(newSkill)
       setNewSkill({name: "", level: 3});
 
     }
@@ -29,9 +20,9 @@ function NewSkillForm(props) {
     return (
       <div>
         <form className="NewSkillForm" onSubmit={handleAdd}>
-            <label htmlFor="Skill">Skill</label>
+            <label htmlFor="name">Skill</label>
             <input 
-              name="Skill"
+              name="name"
               type="text"
               placeholder="New Skill"
               value={newSkill.name}
@@ -39,8 +30,8 @@ function NewSkillForm(props) {
               required
               pattern=".{3,}"
             />
-            <label htmlFor="Level" value="">Level</label>
-            <select name="Level" id="" value={newSkill.level} onChange={handleChange}>
+            <label htmlFor="level" value="">Level</label>
+            <select name="level" id="" value={newSkill.level} onChange={handleChange}>
                 <option value={1}>1</option>
                 <option value={2}>2</option>
                 <option value={3}>3</option>
